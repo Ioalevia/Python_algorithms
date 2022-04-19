2aaed580f839ac99957abadbd7eec5007475d30a
Matsvei <stihyary@gmail.com>
2022-04-19 22:04:13 +0300

    Petrovski Matsvei lesson_1

---
+++ lesson_1_6.py
@@ -0,0 +1,6 @@
+'''https://drive.google.com/file/d/1_I_aVj1ddiPIIfxWHIySb282DB1xmfrB/view?usp=sharing'''
+
+a = int(input('Введите номер буквы в латинском алфавите (1-26): '))
+b = chr(a + 64)
+c = chr(a + 96)
+print(f'Под цифрой {a} числится буква {b}{c}.')
